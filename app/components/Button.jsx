bce6ebcb88685549feb2a61f05980e5446c56eff
joe <hudakjoe@msn.com>
2022-03-06 23:31:09 -0500

    Update Button.jsx

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
-
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+// zach teaching me how componets and props work
 const styles = StyleSheet.create({
   buttonView: {
     marginHorizontal: 0,
-    justifyContent: "center",
-    backgroundColor: "blue",
+    justifyContent: 'center',
+    backgroundColor: 'blue',
 
     borderRadius: 5,
   },
   text: {
-    color: "white",
+    color: 'white',
   },
 });
 
 export default function Button({ name, onPress, size }) {
   const setSize = function (buttonSize) {
-    if (buttonSize === "large") {
+    if (buttonSize === 'large') {
       return {
         paddingHorizontal: 40,
         paddingVertical: 20,
